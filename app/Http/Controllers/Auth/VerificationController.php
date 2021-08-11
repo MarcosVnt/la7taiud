2ce838d16099f405afdd1f62da3206a44c444a64
Marcos Vnt <marcos.visualnt@gmail.com>
2021-08-11 10:40:22 +0200

    verifica desde otro pc

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -7,6 +7,12 @@ use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
 
+
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Auth\Access\AuthorizationException;
+
+
 class VerificationController extends Controller
 {
     /*
@@ -51,6 +57,10 @@ class VerificationController extends Controller
 
        // dd(Auth::check() ,Auth::user(),(Auth::check() && Auth::user()->role_id == 2));
 
+
+         $this->middleware('signed')->only('verify');
+        $this->middleware('throttle:6,1')->only('verify', 'resend');
+
         if(Auth::check() && Auth::user()->role_id == 1){
 
             
@@ -62,6 +72,28 @@ class VerificationController extends Controller
 
         return redirect()->route('logout');
         $this->middleware('guest')->except('logout');
+
+
+
+    }
+
+    public function verify(Request $request)
+    {
+        //dd($request);
+
+        $user = \App\User::find($request->route('id'));
+
+        auth()->login($user);
+
+        if ($request->user()->hasVerifiedEmail()) {
+            return redirect($this->redirectPath());
+        }
+
+        if ($request->user()->markEmailAsVerified()) {
+            event(new Verified($request->user()));
+        }
+
+        return redirect($this->redirectPath())->with('verified', true);
 
 
 
